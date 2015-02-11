0b22bbcd5120e46384bab0156ccef5067f3069cf
bcopos <bogdan.c.15@gmail.com>
2015-02-11 09:37:54 -0800

    updates

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Android Checkin allows you to register a Google account with a device, as if it 
 
 Most of the code for the checkin process is from [android-checkin](https://github.com/nviennot/android-checkin). However, the original code did not work since Google changed their Authentication process to require encrypted password (vs. plaintext). This repository includes a tool for encrypting Google account passwords in the same fashion as GoogleLoginService.
 
-The password encryption method is the reverse-engineered version of `gapps-jb-20130813-signed/system/app/GoogleLoginService/smali/com/google/android/gsf/loginservice/Password_Encrypter.smali` (aka Android 4.3 JellyBean). It has not been tested on KitKat or Jellybean.
+The password encryption method is the reverse-engineered version of `gapps-jb-20130813-signed/system/app/GoogleLoginService/smali/com/google/android/gsf/loginservice/Password_Encrypter.smali` (aka Android 4.3 JellyBean). It has not been tested on Lollipop but seems to also work for Jellybean (4.4).
+
+The device is mimicks is a MotoX Gen 1. Also added ble as one of the hardware components to enable downloading of more applications.
 
 TODO:
 - figure out why it needs to manually add deps to classpath
@@ -22,7 +24,7 @@ Usage
 From the command line:
 ```shell
 # Outputs the registered android_id
-java -Xbootclasspath/a:lib/httpcore-4.2.2.jar:lib/httpclient-4.2.2.jar:lib/httpmime-4.2.2.jar:lib/httpclient-cache-4.2.2.jar:lib/commons-logging-1.1.1.jar:lib/fluent-hc-4.2.2.jar:lib/protobuf-java-2.4.1.jar -jar android-checkin-jb.jar <email> <password> <androidId>
+java -Xbootclasspath/a:lib/httpcore-4.2.2.jar:lib/httpclient-4.2.2.jar:lib/httpmime-4.2.2.jar:lib/httpclient-cache-4.2.2.jar:lib/commons-logging-1.1.1.jar:lib/fluent-hc-4.2.2.jar:lib/protobuf-java-2.4.1.jar -jar android-checkin-jb.jar <email> <password>
 ```
 
 To obtain androidId via adb
--- src/main/java/com/android/checkin/Checkin.java
+++ src/main/java/com/android/checkin/Checkin.java
@@ -99,7 +99,7 @@ public class Checkin {
         data.add(new BasicNameValuePair("lang",           "en"));
         data.add(new BasicNameValuePair("device_country", "us"));
         data.add(new BasicNameValuePair("operatorCountry","us"));
-        data.add(new BasicNameValuePair("sdk_version",    "18"));
+        data.add(new BasicNameValuePair("sdk_version",    "19"));
 
         this.LSID = postFormFetchValue("https://android.clients.google.com/auth", data, "LSID");
     }
@@ -193,19 +193,18 @@ public class Checkin {
 		    .setDigest("1-929a0dca0eee55513280171a8585da7dcd3700f8")
 		    .setCheckin(AndroidCheckinProto.newBuilder()
 			.setBuild(AndroidBuildProto.newBuilder()
-			    .setId("google/yakju/maguro:4.3/JRO03C/398337:user/release-keys")
-			    .setProduct("tuna")
-			    .setCarrier("Google")
-			    .setRadio("I9250XXLA2")
-			    .setBootloader("PRIMELA03")
+			    .setId("motorola/ghost_verizon/ghost:4.4.4/KXA21.12-L1.26/26:user/release-keys")
+			    .setProduct("qcom")
+			    .setCarrier("motorola")
+			    .setRadio("MSM8960PRO_BP_2323.038.123.00R")
+			    .setBootloader("0x30B7")
 			    .setClient("android-google")
 			    .setTimestamp(new Date().getTime()/1000)
-			    .setGoogleServices(18)
-			    .setDevice("maguro")
-			    .setSdkVersion(18)
-			    .setModel("Galaxy Nexus 4")
-			    .setManufacturer("LG")
-			    .setBuildProduct("yakju")
+			    .setDevice("ghost")
+			    .setSdkVersion(19)
+			    .setModel("XT1060")
+			    .setManufacturer("motorola")
+			    .setBuildProduct("ghost_verizon")
 			    .setOtaInstalled(false))
 			.setLastCheckinMsec(this.lastCheckin)
 			.addEvent(AndroidEventProto.newBuilder()
@@ -214,8 +213,8 @@ public class Checkin {
 			    .setTimeMsec(new Date().getTime()))
 			// stat
 			// requestedGroup
-			.setCellOperator("310260") // T-Mobile
-			.setSimOperator("310260")  // T-Mobile
+			.setCellOperator("311480") // Verizon
+			.setSimOperator("311480")  // Verizon
 			.setRoaming("mobile-notroaming")
 			.setUserNumber(0))
 		    // desiredBuild
@@ -239,16 +238,19 @@ public class Checkin {
 			.setHasHardKeyboard(false)
 			.setHasFiveWayNavigation(false)
 			.setScreenDensity(320)
-			.setGlEsVersion(131072)
+			.setGlEsVersion(19660)
 			.addAllSystemSharedLibrary(Arrays.asList(
-			    "android.test.runner",
 			    "com.android.future.usb.accessory",
 			    "com.android.location.provider",
 			    "com.android.nfc_extras",
 			    "com.google.android.maps",
 			    "com.google.android.media.effects",
 			    "com.google.widevine.software.drm",
-			    "javax.obex"))
+			    "javax.obex",
+			    "org.codeaurora.imslibrary",
+			    "org.simalliance.openmobileapi",
+			    "com.verizon.phone",
+			    ""))
 			.addAllSystemAvailableFeature(Arrays.asList(
 			    "android.hardware.bluetooth",
 			    "android.hardware.camera",
@@ -261,6 +263,8 @@ public class Checkin {
 			    "android.hardware.location.network",
 			    "android.hardware.microphone",
 			    "android.hardware.nfc",
+			    "android.hardware.nfc.hce",
+			    "android.hardware.bluetooth_le",
 			    "android.hardware.screen.landscape",
 			    "android.hardware.screen.portrait",
 			    "android.hardware.sensor.accelerometer",
@@ -271,6 +275,7 @@ public class Checkin {
 			    "android.hardware.sensor.proximity",
 			    "android.hardware.telephony",
 			    "android.hardware.telephony.gsm",
+			    "android.hardware.telephony.cdma",
 			    "android.hardware.touchscreen",
 			    "android.hardware.touchscreen.multitouch",
 			    "android.hardware.touchscreen.multitouch.distinct",
@@ -282,11 +287,11 @@ public class Checkin {
 			    "android.software.live_wallpaper",
 			    "android.software.sip",
 			    "android.software.sip.voip",
-			    "com.cyanogenmod.android",
-			    "com.cyanogenmod.nfc.enhanced",
-			    "com.google.android.feature.GOOGLE_BUILD",
-			    "com.nxp.mifare",
-			    "com.tmobile.software.themes"))
+			    "android.software.app_widgets",
+			    "android.software.device_admin",
+			    "android.software.home_screen",
+			    "android.software.input_methods",
+			    "com.nxp.mifare"))
 			.addAllNativePlatform(Arrays.asList(
 			    "armeabi-v7a",
 			    "armeabi"))
@@ -308,58 +313,69 @@ public class Checkin {
 			    "th_TH", "tl", "tl_PH", "tr", "tr_TR", "ug", "ug_CN", "uk",
 			    "uk_UA", "vi", "vi_VN", "zh_CN", "zh_TW", "zu", "zu_ZA"))
 			.addAllGlExtension(Arrays.asList(
+			    "GL_AMD_compressed_ATC_texture",
+			    "GL_AMD_performance_monitor",
+			    "GL_AMD_program_binary_Z400",
+			    "GL_APPLE_texture_2D_limited_npot",
+			    "GL_ARB_vertex_buffer_object",
+			    "GL_EXT_color_buffer_float",
+			    "GL_EXT_color_buffer_half_float",
+			    "GL_EXT_debug_label",
 			    "GL_EXT_debug_marker",
 			    "GL_EXT_discard_framebuffer",
-			    "GL_EXT_multi_draw_arrays",
-			    "GL_EXT_shader_texture_lod",
+			    "GL_EXT_disjoint_timer_query",
+			    "GL_EXT_multisampled_render_to_texture",
+			    "GL_EXT_robustness",
+			    "GL_EXT_sRGB",
+			    "GL_EXT_sRGB_write_control",
+			    "GL_EXT_texture_filter_anisotropic",
 			    "GL_EXT_texture_format_BGRA8888",
-			    "GL_IMG_multisampled_render_to_texture",
-			    "GL_IMG_program_binary",
-			    "GL_IMG_read_format",
-			    "GL_IMG_shader_binary",
-			    "GL_IMG_texture_compression_pvrtc",
-			    "GL_IMG_texture_format_BGRA8888",
-			    "GL_IMG_texture_npot",
-			    "GL_IMG_vertex_array_object",
+			    "GL_EXT_texture_sRGB_decode",
+			    "GL_EXT_texture_type_2_10_10_10_REV",
+			    "GL_NV_fence",
 			    "GL_OES_EGL_image",
 			    "GL_OES_EGL_image_external",
 			    "GL_OES_blend_equation_separate",
 			    "GL_OES_blend_func_separate",
 			    "GL_OES_blend_subtract",
-			    "GL_OES_byte_coordinates",
 			    "GL_OES_compressed_ETC1_RGB8_texture",
 			    "GL_OES_compressed_paletted_texture",
 			    "GL_OES_depth24",
 			    "GL_OES_depth_texture",
+			    "GL_OES_depth_texture_cube_map",
 			    "GL_OES_draw_texture",
-			    "GL_OES_egl_sync",
 			    "GL_OES_element_index_uint",
-			    "GL_OES_extended_matrix_palette",
-			    "GL_OES_fixed_point",
+			    "GL_OES_fbo_render_mipmap",
 			    "GL_OES_fragment_precision_high",
 			    "GL_OES_framebuffer_object",
 			    "GL_OES_get_program_binary",
-			    "GL_OES_mapbuffer",
-			    "GL_OES_matrix_get",
 			    "GL_OES_matrix_palette",
 			    "GL_OES_packed_depth_stencil",
 			    "GL_OES_point_size_array",
 			    "GL_OES_point_sprite",
-			    "GL_OES_query_matrix",
 			    "GL_OES_read_format",
-			    "GL_OES_required_internalformat",
 			    "GL_OES_rgb8_rgba8",
-			    "GL_OES_single_precision",
 			    "GL_OES_standard_derivatives",
-			    "GL_OES_stencil8",
 			    "GL_OES_stencil_wrap",
+			    "GL_OES_texture_3D",
 			    "GL_OES_texture_cube_map",
 			    "GL_OES_texture_env_crossbar",
 			    "GL_OES_texture_float",
 			    "GL_OES_texture_half_float",
+			    "GL_OES_texture_half_float_linear",
 			    "GL_OES_texture_mirrored_repeat",
+			    "GL_OES_texture_npot",
 			    "GL_OES_vertex_array_object",
-			    "GL_OES_vertex_half_float")))
+			    "GL_OES_vertex_half_float",
+			    "GL_OES_vertex_type_10_10_10_2",
+			    "GL_QCOM_alpha_test",
+			    "GL_QCOM_binning_control",
+			    "GL_QCOM_driver_control",
+			    "GL_QCOM_extended_get",
+			    "GL_QCOM_extended_get2",
+			    "GL_QCOM_perfmon_global_mode",
+			    "GL_QCOM_tiled_rendering",
+			    "GL_QCOM_writeonly_rendering")))
 			// deviceClass
 			// maxApkDownloadSizeMb
 		    .addMacAddrType("wifi")
@@ -376,19 +392,18 @@ public class Checkin {
 		    .setDigest("1-929a0dca0eee55513280171a8585da7dcd3700f8")
 		    .setCheckin(AndroidCheckinProto.newBuilder()
 			.setBuild(AndroidBuildProto.newBuilder()
-			    .setId("google/yakju/maguro:4.1.1/JRO03C/398337:user/release-keys")
-			    .setProduct("tuna")
-			    .setCarrier("Google")
-			    .setRadio("I9250XXLA2")
-			    .setBootloader("PRIMELA03")
+			    .setId("motorola/ghost_verizon/ghost:4.4.4/KXA21.12-L1.26/26:user/release-keys")
+			    .setProduct("qcom")
+			    .setCarrier("motorola")
+			    .setRadio("MSM8960PRO_BP_2323.038.123.00R")
+			    .setBootloader("0x30B7")
 			    .setClient("android-google")
 			    .setTimestamp(new Date().getTime()/1000)
-			    .setGoogleServices(18)
-			    .setDevice("maguro")
-			    .setSdkVersion(18)
-			    .setModel("Galaxy Nexus")
-			    .setManufacturer("Samsung")
-			    .setBuildProduct("yakju")
+			    .setDevice("ghost")
+			    .setSdkVersion(19)
+			    .setModel("XT1060")
+			    .setManufacturer("motorola")
+			    .setBuildProduct("ghost_verizon")
 			    .setOtaInstalled(false))
 			.setLastCheckinMsec(0)
 			.addEvent(AndroidEventProto.newBuilder()
@@ -397,8 +412,8 @@ public class Checkin {
 			    .setTimeMsec(this.lastCheckin))
 			// stat
 			// requestedGroup
-			.setCellOperator("310260") // T-Mobile
-			.setSimOperator("310260")  // T-Mobile
+			.setCellOperator("311480") // Verizon
+			.setSimOperator("311480")  // Verizon
 			.setRoaming("mobile-notroaming")
 			.setUserNumber(0))
 		    // desiredBuild
@@ -421,16 +436,19 @@ public class Checkin {
 			.setHasHardKeyboard(false)
 			.setHasFiveWayNavigation(false)
 			.setScreenDensity(320)
-			.setGlEsVersion(131072)
+			.setGlEsVersion(19660)
 			.addAllSystemSharedLibrary(Arrays.asList(
-			    "android.test.runner",
 			    "com.android.future.usb.accessory",
 			    "com.android.location.provider",
 			    "com.android.nfc_extras",
 			    "com.google.android.maps",
 			    "com.google.android.media.effects",
 			    "com.google.widevine.software.drm",
-			    "javax.obex"))
+			    "javax.obex",
+			    "org.codeaurora.imslibrary",
+			    "org.simalliance.openmobileapi",
+			    "com.verizon.phone",
+			    ""))
 			.addAllSystemAvailableFeature(Arrays.asList(
 			    "android.hardware.bluetooth",
 			    "android.hardware.camera",
@@ -443,6 +461,8 @@ public class Checkin {
 			    "android.hardware.location.network",
 			    "android.hardware.microphone",
 			    "android.hardware.nfc",
+			    "android.hardware.nfc.hce",
+			    "android.hardware.bluetooth_le",
 			    "android.hardware.screen.landscape",
 			    "android.hardware.screen.portrait",
 			    "android.hardware.sensor.accelerometer",
@@ -464,11 +484,10 @@ public class Checkin {
 			    "android.software.live_wallpaper",
 			    "android.software.sip",
 			    "android.software.sip.voip",
-			    "com.cyanogenmod.android",
-			    "com.cyanogenmod.nfc.enhanced",
-			    "com.google.android.feature.GOOGLE_BUILD",
-			    "com.nxp.mifare",
-			    "com.tmobile.software.themes"))
+			    "android.software.app_widgets",
+			    "android.software.device_admin",
+			    "android.software.home_screen",
+			    "android.software.input_methods"))
 			.addAllNativePlatform(Arrays.asList(
 			    "armeabi-v7a",
 			    "armeabi"))
@@ -490,58 +509,69 @@ public class Checkin {
 			    "th_TH", "tl", "tl_PH", "tr", "tr_TR", "ug", "ug_CN", "uk",
 			    "uk_UA", "vi", "vi_VN", "zh_CN", "zh_TW", "zu", "zu_ZA"))
 			.addAllGlExtension(Arrays.asList(
+			    "GL_AMD_compressed_ATC_texture",
+			    "GL_AMD_performance_monitor",
+			    "GL_AMD_program_binary_Z400",
+			    "GL_APPLE_texture_2D_limited_npot",
+			    "GL_ARB_vertex_buffer_object",
+			    "GL_EXT_color_buffer_float",
+			    "GL_EXT_color_buffer_half_float",
+			    "GL_EXT_debug_label",
 			    "GL_EXT_debug_marker",
 			    "GL_EXT_discard_framebuffer",
-			    "GL_EXT_multi_draw_arrays",
-			    "GL_EXT_shader_texture_lod",
+			    "GL_EXT_disjoint_timer_query",
+			    "GL_EXT_multisampled_render_to_texture",
+			    "GL_EXT_robustness",
+			    "GL_EXT_sRGB",
+			    "GL_EXT_sRGB_write_control",
+			    "GL_EXT_texture_filter_anisotropic",
 			    "GL_EXT_texture_format_BGRA8888",
-			    "GL_IMG_multisampled_render_to_texture",
-			    "GL_IMG_program_binary",
-			    "GL_IMG_read_format",
-			    "GL_IMG_shader_binary",
-			    "GL_IMG_texture_compression_pvrtc",
-			    "GL_IMG_texture_format_BGRA8888",
-			    "GL_IMG_texture_npot",
-			    "GL_IMG_vertex_array_object",
+			    "GL_EXT_texture_sRGB_decode",
+			    "GL_EXT_texture_type_2_10_10_10_REV",
+			    "GL_NV_fence",
 			    "GL_OES_EGL_image",
 			    "GL_OES_EGL_image_external",
 			    "GL_OES_blend_equation_separate",
 			    "GL_OES_blend_func_separate",
 			    "GL_OES_blend_subtract",
-			    "GL_OES_byte_coordinates",
 			    "GL_OES_compressed_ETC1_RGB8_texture",
 			    "GL_OES_compressed_paletted_texture",
 			    "GL_OES_depth24",
 			    "GL_OES_depth_texture",
+			    "GL_OES_depth_texture_cube_map",
 			    "GL_OES_draw_texture",
-			    "GL_OES_egl_sync",
 			    "GL_OES_element_index_uint",
-			    "GL_OES_extended_matrix_palette",
-			    "GL_OES_fixed_point",
+			    "GL_OES_fbo_render_mipmap",
 			    "GL_OES_fragment_precision_high",
 			    "GL_OES_framebuffer_object",
 			    "GL_OES_get_program_binary",
-			    "GL_OES_mapbuffer",
-			    "GL_OES_matrix_get",
 			    "GL_OES_matrix_palette",
 			    "GL_OES_packed_depth_stencil",
 			    "GL_OES_point_size_array",
 			    "GL_OES_point_sprite",
-			    "GL_OES_query_matrix",
 			    "GL_OES_read_format",
-			    "GL_OES_required_internalformat",
 			    "GL_OES_rgb8_rgba8",
-			    "GL_OES_single_precision",
 			    "GL_OES_standard_derivatives",
-			    "GL_OES_stencil8",
 			    "GL_OES_stencil_wrap",
+			    "GL_OES_texture_3D",
 			    "GL_OES_texture_cube_map",
 			    "GL_OES_texture_env_crossbar",
 			    "GL_OES_texture_float",
 			    "GL_OES_texture_half_float",
+			    "GL_OES_texture_half_float_linear",
 			    "GL_OES_texture_mirrored_repeat",
+			    "GL_OES_texture_npot",
 			    "GL_OES_vertex_array_object",
-			    "GL_OES_vertex_half_float")))
+			    "GL_OES_vertex_half_float",
+			    "GL_OES_vertex_type_10_10_10_2",
+			    "GL_QCOM_alpha_test",
+			    "GL_QCOM_binning_control",
+			    "GL_QCOM_driver_control",
+			    "GL_QCOM_extended_get",
+			    "GL_QCOM_extended_get2",
+			    "GL_QCOM_perfmon_global_mode",
+			    "GL_QCOM_tiled_rendering",
+			    "GL_QCOM_writeonly_rendering")))
 			// deviceClass
 			// maxApkDownloadSizeMb
 		    .addMacAddrType("wifi")
